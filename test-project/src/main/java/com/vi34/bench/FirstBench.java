409c8d76200808fba4e5301a5bb22cf21c4a5034
Viktor <viteg34@yandex.ru>
2017-02-16 14:27:54 +0300

    create first benchmark

--- test-project/src/main/java/com/vi34/bench/FirstBench.java
+++ test-project/src/main/java/com/vi34/bench/FirstBench.java
@@ -1,25 +1,65 @@
 package com.vi34.bench;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.vi34.PojoSerializer;
+import com.vi34.entities.Pojo;
+import com.vi34.entities.Pojo2;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by vi34 on 16/02/2017.
  */
+@State(Scope.Thread)
 public class FirstBench {
 
-    @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+    ObjectMapper mapper;
+    MappingJsonFactory factory;
+    Pojo pojo;
+    Pojo2 pojo2;
 
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
+    public FirstBench() {
+        factory = new MappingJsonFactory();
+        factory.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+
+
     }
+
+    @Setup(Level.Trial)
+    public void setup() {
+        mapper = new ObjectMapper(factory);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Pojo.class, new PojoSerializer());
+        mapper.registerModule(module);
+        pojo = new Pojo(12345, "pojo", Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9 ,0));
+        pojo2 = new Pojo2(12345, "pojo2", Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 ,9 ,0));
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public String customSerializer() throws JsonProcessingException {
+        return mapper.writeValueAsString(pojo);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public String reflection() throws JsonProcessingException {
+        return mapper.writeValueAsString(pojo2);
+    }
+
 
 
     public static void main(String[] args) throws RunnerException {
