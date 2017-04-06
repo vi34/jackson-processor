757ba885d5b66f6ce1ac2f71099916683df10613
Viktor <viteg34@yandex.ru>
2017-04-06 15:18:12 +0300

    Junit

--- src/main/test/com/vi34/Compilation.java
+++ src/main/test/com/vi34/Compilation.java
@@ -9,13 +9,15 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.vi34.raw.Complex;
 import com.vi34.raw.Pojo;
 import com.vi34.raw.WithString;
-import org.junit.jupiter.api.Assertions;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 
 import javax.tools.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
@@ -25,19 +27,16 @@ import java.util.Arrays;
 public class Compilation {
 
     public static final String TARGET_DIR = "./target/generated-sources/annotations/";
-    private static File targetFile = new File(TARGET_DIR);
-    static MappingJsonFactory factory = new MappingJsonFactory();
-    static ObjectMapper mapper = new ObjectMapper(factory);
+    public static File targetFile = new File(TARGET_DIR);
     static URLClassLoader classLoader;
-    private static SimpleModule module;
-
 
     public static void compile(File... files) throws IOException {
+        FileUtils.deleteDirectory(targetFile);
+        targetFile.mkdir();
         final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         final DiagnosticCollector< JavaFileObject > diagnostics = new DiagnosticCollector<>();
         final StandardJavaFileManager manager = compiler.getStandardFileManager(diagnostics, null, null );
         final Iterable< ? extends JavaFileObject> sources = manager.getJavaFileObjectsFromFiles(Arrays.asList(files));
-        classLoader = new URLClassLoader(new URL[]{targetFile.toURI().toURL()});
         manager.setLocation(StandardLocation.SOURCE_OUTPUT,
                 Arrays.asList(targetFile));
         manager.setLocation(StandardLocation.CLASS_OUTPUT,
@@ -60,31 +59,9 @@ public class Compilation {
         manager.close();
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    static <T> boolean load(Class<? extends T> type, ObjectMapper mapper) {
         try {
-            setUp();
-            pojo();
-            complex();
-            withString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
-    @SuppressWarnings("unchecked")
-    public static void setUp() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        factory.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
-        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-        compile(Paths.get("./src/main/test/com/vi34/raw/").toFile().listFiles());
-        module = new SimpleModule();
-        //load("com.vi34.PojoSerializer", module, Pojo.class);
-        mapper.registerModule(module);
-    }
-
-    private static <T> boolean load(String className, Class<? extends T> type) {
-        try {
+            String className = "com.vi34." + type.getSimpleName() + SerializerGenerator.SUFFIX;
             SimpleModule module = new SimpleModule();
             module.addSerializer(type ,(StdSerializer<T>) classLoader.loadClass(className).newInstance());
             mapper.registerModule(module);
@@ -93,44 +70,5 @@ public class Compilation {
             System.out.println(type.toString() + " load failed");
             return false;
         }
-    }
-
-    public static void withString() throws IOException {
-        if (!load("com.vi34.WithStringSerializer", WithString.class)) {
-            return;
-        }
-        WithString val = new WithString(1,1.2, "str", "name");
-
-        mapper.writeValue(new File("tmp"), val);
-        WithString read = mapper.readValue(new File("tmp"), WithString.class);
-        Assertions.assertEquals(val, read);
-
-        System.out.println("WithString ok");
-    }
-
-    public static void pojo() throws IOException {
-        if (!load("com.vi34.PojoSerializer", Pojo.class)) {
-            return;
-        }
-        Pojo pojo = new Pojo(13, 0.4);
-
-        mapper.writeValue(new File("tmp"), pojo);
-        Pojo read = mapper.readValue(new File("tmp"), Pojo.class);
-        Assertions.assertEquals(pojo, read);
-
-        System.out.println("Pojo ok");
-    }
-
-    public static void complex() throws IOException {
-        if (!load("com.vi34.ComplexSerializer", Complex.class)) {
-            return;
-        }
-        Complex complex = new Complex(1, new Pojo(1, 2));
-
-        mapper.writeValue(new File("tmp"), complex);
-        Complex read = mapper.readValue(new File("tmp"), Complex.class);
-        Assertions.assertEquals(complex, read);
-
-        System.out.println("Complex ok");
     }
 }
---
+++ src/main/test/com/vi34/JUnitCompilation.java
@@ -0,0 +1,72 @@
+package com.vi34;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.vi34.raw.Complex;
+import com.vi34.raw.Pojo;
+import com.vi34.raw.WithString;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
+
+import static com.vi34.Compilation.load;
+
+/**
+ * Created by vi34 on 18/02/2017.
+ */
+public class JUnitCompilation {
+
+    private static MappingJsonFactory factory = new MappingJsonFactory();
+    private static ObjectMapper mapper = new ObjectMapper(factory);
+
+    @SuppressWarnings("unchecked")
+    @BeforeClass
+    public static void setUp() throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+        factory.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        Compilation.classLoader = new URLClassLoader(new URL[]{Compilation.targetFile.toURI().toURL()});
+        Compilation.compile(Paths.get("./src/main/test/com/vi34/raw/").toFile().listFiles());
+    }
+
+    @Test
+    public void withString() throws IOException {
+        Assert.assertTrue(load(WithString.class, mapper));
+
+        WithString val = new WithString(1, 1.2, "str", "name");
+
+        mapper.writeValue(new File("tmp"), val);
+        WithString read = mapper.readValue(new File("tmp"), WithString.class);
+        Assert.assertEquals(val, read);
+    }
+
+
+    @Test
+    public void pojo() throws IOException {
+        Assert.assertTrue(load(Pojo.class, mapper));
+
+        Pojo pojo = new Pojo(13, 0.4);
+
+        mapper.writeValue(new File("tmp"), pojo);
+        Pojo read = mapper.readValue(new File("tmp"), Pojo.class);
+        Assert.assertEquals(pojo, read);
+    }
+
+    @Test
+    public void complex() throws IOException {
+        Assert.assertTrue(load(Complex.class, mapper));
+
+        Complex complex = new Complex(1, new Pojo(1, 2));
+
+        mapper.writeValue(new File("tmp"), complex);
+        Complex read = mapper.readValue(new File("tmp"), Complex.class);
+        Assert.assertEquals(complex, read);
+    }
+}
